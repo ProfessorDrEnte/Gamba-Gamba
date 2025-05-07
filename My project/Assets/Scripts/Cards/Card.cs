2b27cb91c496ab4dab4fdbdca35ea3842d6ca1bb
Prof. Dr. Ente <95550209+ProfessorDrEnte@users.noreply.github.com>
2025-05-07 22:25:29 +0200

    Card Datastructure added

--- My project/Assets/Scripts/Cards/Card.cs
+++ My project/Assets/Scripts/Cards/Card.cs
@@ -1,6 +1,41 @@
 using UnityEngine;
 
+public enum Suit
+{
+    Hearts, Diamonds, Clubs, Spades
+}
+
+public enum Rank
+{
+    Two = 2,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    Ace
+}
+
 public class Card
 {
-    
+    public int Rank Rank { get; set; }
+    public string Suit Suit { get; set; }
+
+        public Card(Suit suit, Rank rank)
+    {
+        this.Suit = suit;
+        this.Rank = rank;
+    }
+
+public override string ToString()
+{
+    return $"{Rank} of {Suit}";
 }
+}
+
